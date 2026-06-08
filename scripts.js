32bb7797baa836d2ff59003232c9ab453a1ab94e
WendelGabriell <gabriel.wendel733@gmail.com>
2026-06-08 00:19:37 -0300

    feat: Adicionando funcionalidades de marcar tarefa como completa e apagar tarefa

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
-const taskContainer = document.querySelector(".tasks-container");
+const tasksContainer = document.querySelector(".tasks-container");
 
 const validateInput = () => inputElement.value.trim().length > 0;
 
@@ -11,22 +11,53 @@ const handleAddTask = () => {
     return inputElement.classList.add("error");
   }
 
-  const taskContainerItem = document.createElement("div");
-  taskContainerItem.classList.add("task-item");
+  const taskItemContainer = document.createElement("div");
+  taskItemContainer.classList.add("task-item");
 
-  taskContainer.appendChild(taskContainerItem);
+  tasksContainer.appendChild(taskItemContainer);
 
   const taskContent = document.createElement("p");
   taskContent.innerText = inputElement.value;
+
+  taskContent.addEventListener("click", () => handleClick(taskContent));
 
   const deleteItem = document.createElement("i");
   deleteItem.classList.add("fa-regular");
   deleteItem.classList.add("fa-trash-can");
 
-  taskContainerItem.appendChild(taskContent);
-  taskContainerItem.appendChild(deleteItem);
+  deleteItem.addEventListener("click", () =>
+    handleDeleteClick(taskItemContainer, taskContent),
+  );
+
+  taskItemContainer.appendChild(taskContent);
+  taskItemContainer.appendChild(deleteItem);
+
+  inputElement.value = "";
 };
 
+const handleClick = (taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentIsBeingClicked = task.firstChild.isSameNode(taskContent);
+
+    if (currentIsBeingClicked) {
+      task.firstChild.classList.toggle("completed");
+    }
+  }
+};
+
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
+
+    if (currentTaskIsBeingClicked) {
+      taskItemContainer.remove();
+    }
+  }
+};
 const handleAddTaskChange = () => {
   const inputIsValid = validateInput();
 
